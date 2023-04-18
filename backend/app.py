e8a9eb53bacfcd8134aaa9337ce0f2d7b3f1923a
CadeChabra <cchabra@email.unc.edu>
2023-04-18 17:36:20 -0400

    Added password encryption/decryption for passwords owned by user

--- backend/app.py
+++ backend/app.py
@@ -157,7 +157,7 @@ def authenticate():
                     # Calculate user's master key
                     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=64, salt=key_salt, iterations=100000)
                     master_key = kdf.derive(password.encode('utf-8'))
-                    # Calculate user's private RSA key and encode it in PEM format
+                    # Calculate user's private RSA key
                     pycrypto_private_key = RSA.generate(2048, PRNG(master_key))
                     private_key_pem = pycrypto_private_key.export_key()
                     # Store the user ID, master key, and private key in session variables to avoid recomputation cost
@@ -172,9 +172,29 @@ def authenticate():
 def get_user(user_id):
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(GET_PASSWORDS, (user_id, ))
-            passwords = cursor.fetchall()
-    return {"passwords": passwords, "message": "Passwords fetched successfully"}, 200
+            if "user_id" in session and session["user_id"] == user_id:
+                # Fetch passwords and create a copy
+                cursor.execute(GET_PASSWORDS, (user_id, ))
+                passwords = cursor.fetchall()
+                decrypted_passwords = passwords
+                # Fetch decryption keys
+                master_key = b64decode(session["master_key"].encode('utf-8'))
+                aes = AESSIV(master_key)
+                private_key_pem = session["private_key"].encode('ascii')
+                private_key = serialization.load_pem_private_key(private_key_pem, None)
+                # Decrypt passwords
+                for i in range(len(passwords)):
+                    # Cast entries from tuples to lists
+                    decrypted_passwords[i] = list(passwords[i])
+                    if passwords[i][3] == True:
+                        # Decrypt owned password with the master AES key
+                        decrypted_passwords[i][2] = aes.decrypt(b64decode(passwords[i][2].encode('utf-8')), None).decode('utf-8')
+                    else:
+                        # Decrypt shared password with private RSA key
+                        decrypted_passwords[i][2] = private_key.decrypt(b64decode(passwords[i][2].encode('utf-8')), padding.OAEP(padding.MGF1(hashes.SHA256()), hashes.SHA256(), None))
+                return {"passwords": decrypted_passwords, "message": "Passwords fetched successfully"}, 200
+            else:
+                return {"message": "User not logged in"}, 401
 
 # Adds a credential to the user's password table
 @app.post("/api/users/<int:user_id>/add")
@@ -183,8 +203,15 @@ def add_credential(user_id):
     website, alias, password =  data["website"], data["alias"], data["password"]
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_PASSWORDS, (user_id, website, alias, password, True, f"{{}}", ))
-    return {"message": f"Password for {website} inserted"}, 200
+            if "user_id" in session and session["user_id"] == user_id:
+                # Encrypt the user's password with the master key before adding to the table
+                master_key = b64decode(session["master_key"].encode('utf-8'))
+                aes = AESSIV(master_key)
+                encrypted_password = aes.encrypt(password.encode('utf-8'), None)
+                cursor.execute(INSERT_PASSWORDS, (user_id, website, alias, b64encode(encrypted_password).decode('utf-8'), True, f"{{}}"))
+                return {"message": f"Password for {website} inserted"}, 200
+            else:
+                return {"message": "User not logged in"}, 401
 
 # Updates a credential in the user's password table
 @app.post("/api/users/<int:user_id>/update")
@@ -194,7 +221,11 @@ def update_credential(user_id):
     with connection:
         with connection.cursor() as cursor:
             # TODO: only allow update if user is owner of password
-            cursor.execute(UPDATE_PASSWORD, (user_id, password, website))
+            # Encrypt the user's password with the master key before adding to the table
+            master_key = b64decode(session["master_key"].encode('utf-8'))
+            aes = AESSIV(master_key)
+            encrypted_password = aes.encrypt(password.encode('utf-8'), None)
+            cursor.execute(UPDATE_PASSWORD, (user_id, b64encode(encrypted_password).decode('utf-8'), website))
             # TODO: update password in shared users' tables
     return {"message": f"Password for {website} updated"}, 200
 
